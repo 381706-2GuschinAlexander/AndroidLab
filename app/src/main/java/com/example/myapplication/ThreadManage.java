75f947a2ae67fd3dc1abca85fc39716ee91e0a7c
Guschin Alexandr <igushin1999@gmail.com>
2020-12-24 01:12:36 +0300

    flavour

--- app/src/main/java/com/example/myapplication/ThreadManage.java
+++ app/src/main/java/com/example/myapplication/ThreadManage.java
@@ -18,11 +18,9 @@ public class ThreadManage {
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
-    Bitmap filter(int type) throws InterruptedException {
+    void filter(int type) throws InterruptedException {
         int width = source.getWidth();
         int height = source.getHeight();
-        //int start[] = {0, width/4, width/4 * 2, width/4 * 3};
-        //int end[] = {width/4, width/4 * 2, width/4 * 3, width/4 * 4 + width%4};
         int[] intArray = new int[width * height];
         source.getPixels(intArray, 0, width, 0, 0, width, height);
         int start[] = {0, width/8, width/8 * 2, width/8 * 3, width/8 * 4, width/8 * 5, width/8 * 6, width/8 * 7};
@@ -37,8 +35,5 @@ public class ThreadManage {
         for(int i = 0; i < 8; ++i)
             threads[i].join();
         dest.setPixels(intArray, 0, width, 0, 0, width, height);
-        //dest.copyPixelsFromBuffer(IntBuffer.wrap(intArray));
-
-        return dest;
     }
 }
